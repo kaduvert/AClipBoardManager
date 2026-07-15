f3ce299fc1a8f962e465dccad4a3cc90fbb52ad9
kaduvert <>
2026-07-15 16:40:11 +0200

    proper icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.clipvault.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     signingConfigs {
